7436e39ea7e114ddb5089840ac74e4662df9c898
ajj5104 <ajj5104@rit.edu>
2026-02-02 17:14:24 -0500

    Refactored DynamicArray for template

--- DynamicArray/DynamicArray.h
+++ DynamicArray/DynamicArray.h
@@ -2,53 +2,529 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
 class DynamicArray
 {
 private:
-	int* start_pos;		// always points to the first index of memory
+	T* start_pos;		// always points to the first index of memory
 	int _size;			// the current max size of the array
 	int _quantity;		// the number of elements in the current array
-	int* access_ptr;	// used to obtain the value at specific indicies
+	T* access_ptr;	// used to obtain the value at specific indicies
 
-	// Internal Functions
-	void MergeRange(int left, int mid, int right, DynamicArray& temp);
-	void MergeSortRange(int left, int right, DynamicArray& temp);
+// Internal Functions
+	/// <summary>
+	/// Merges the two sides of the array
+	/// </summary>
+	/// <param name="left">
+	/// The lower bound
+	/// </param>
+	/// <param name="mid">
+	/// The middle bound
+	/// </param>
+	/// <param name="right">
+	/// The upper bound
+	/// </param>
+	/// <param name="temp">
+	/// The temp buffer
+	/// </param>
+	void MergeRange(int left, int mid, int right, DynamicArray<T>& temp) {
+		int i = left, j = mid + 1, k = left;
+
+		while (i <= mid && j <= right) {
+			if (this->FindAtIndex(i) <= this->FindAtIndex(j)) {
+				temp[k] = this->FindAtIndex(i);
+				i++;
+			}
+			else {
+				temp[k] = this->FindAtIndex(j);
+				j++;
+			}
+			k++;
+		}
+
+		while (i <= mid) {
+			temp[k] = this->FindAtIndex(i);
+			i++, k++;
+		}
+		while (j <= right) {
+			temp[k] = this->FindAtIndex(j);
+			j++, k++;
+		}
+
+		for (int l = left; l <= right; l++) {
+			this->ReplaceElement(l, temp[l]);
+		}
+	}
+
+	/// <summary>
+	/// Does the merge sort on each side of the array in much quicker time by using a temp buffer
+	/// </summary>
+	/// <param name="left">
+	/// The lower bound of the array
+	/// </param>
+	/// <param name="right">
+	/// The upper bound of the array
+	/// </param>
+	/// <param name="temp">
+	/// The temp buffer
+	/// </param>
+	void MergeSortRange(int left, int right, DynamicArray& temp) {
+		if (left >= right) return;
+		int mid = left + (right - left) / 2;
+
+		MergeSortRange(left, mid, temp);
+		MergeSortRange(mid + 1, right, temp);
+		MergeRange(left, mid, right, temp);
+	}
 
 public:
-	// Basic Class Overhead
-	DynamicArray();
-	DynamicArray(int size);
-	DynamicArray(const DynamicArray& other);
-	DynamicArray& operator=(const DynamicArray& other);
-	~DynamicArray();
+// Basic Class Overhead
+	/// <summary>
+	/// Initializes the array to a size of 1 with no elements inside.
+	/// Sets all pointers to the start of the array.
+	/// </summary>
+	DynamicArray() {
+		start_pos = new T[1], _size = 1, _quantity = 0, access_ptr = start_pos;
+	}
+	
+	/// <summary>
+	/// Initializes the array to a user defined size with no elements inside.
+	/// Sets all pointers to the start of the array.
+	/// </summary>
+	/// <param name="size">
+	/// The size of the array to be created.
+	/// </param>
+	DynamicArray(int size) {
+		start_pos = new T[size], _size = size, _quantity = 0, access_ptr = start_pos;
+	}
+	
+	/// <summary>
+	/// Copy constructor for class
+	/// </summary>
+	/// <param name="other">
+	/// The array you want to copy from for construction
+	/// </param>
+	DynamicArray(const DynamicArray<T>& other) {
+		_size = other._size;
+		_quantity = other._quantity;
 
-	// Size Manipulation, Insertion, Removals
-	void IncreaseSize();
-	void PushBack(int val);
-	void PopBack();
-	void Remove(int idx);
-	void SwapIndices(int start_idx, int end_idx);
-	void Insert(int idx, int val);
+		start_pos = new T[_size];
+		for (int i = 0; i < _quantity; i++) start_pos[i] = other.start_pos[i];
 
-	// Element Access Operations
-	int FindAtIndex(int idx);
-	void ReplaceElement(int idx, int val);
-	int& operator[](int idx);
-	int FindElement(int val);
-	bool Contains(int val);
+		access_ptr = start_pos;
+	}
+	
+	/// <summary>
+	/// Allows for dynamic arrays to be set equal to each other
+	/// </summary>
+	/// <param name="other">
+	/// The array you are copying from
+	/// </param>
+	/// <returns>
+	/// The copied dynamic array
+	/// </returns>
+	DynamicArray& operator=(const DynamicArray<T>& other) {
+		if (this == &other) return *this;
 
-	// Internal Class Data Accessors
-	int GetSize();
-	int GetQuantity();
-	int* GetPointer(int idx);
-	bool IsEmpty();
-	void Clear();
-	void Print();
+		delete[] start_pos;
+		_size = other._size;
+		_quantity = other._quantity;
 
-	// Sorting Algorithms
-	void MergeSort();
-	void InsertionSort();
-	void BubbleSort();
-	void SelectionSort();
-	void QuickSort();
+		start_pos = new T[_size];
+		for (int i = 0; i < _quantity; i++) start_pos[i] = other.start_pos[i];
+
+		access_ptr = start_pos;
+		return *this;
+	}
+	
+	/// <summary>
+	/// Destructor for the dynamic array.
+	/// </summary>
+	~DynamicArray() {
+		delete[] start_pos;
+		start_pos = nullptr;
+		access_ptr = nullptr;
+		_size = 0;
+		_quantity = 0;
+	}
+
+// Size Manipulation, Insertion, Removals
+	/// <summary>
+	/// Increases the size of the array by one.
+	/// </summary>
+	void IncreaseSize() {
+		T* new_array = new T[_size + 1];
+		T* new_mov = new_array;
+		access_ptr = start_pos;
+		int i = 0;
+
+		while (i < _size) {
+			*new_mov = *access_ptr;
+			new_mov++, access_ptr++, i++;
+		}
+
+		delete[] start_pos;
+		start_pos = new_array;
+		access_ptr = start_pos;
+
+		_size++;
+	}
+	
+	/// <summary>
+	/// Adds the value from the user into the array.
+	/// Calls upon the increase size function to fix sizing issues (if needed)
+	/// </summary>
+	/// <param name="val">
+	/// The value to be added to the array.
+	/// </param>
+	void PushBack(const T& val) {
+		if (_quantity >= _size) IncreaseSize();
+		*(start_pos + _quantity) = val;
+		_quantity++;
+	}
+	
+	/// <summary>
+	/// Removes the last element from the array and decreases the size by 1.
+	/// </summary>
+	void PopBack() {
+		if (this->GetQuantity() == 0) return;
+		_quantity--;
+		T* new_array = new T[_quantity];
+		T* new_mov = new_array;
+		access_ptr = start_pos;
+		int i = 0;
+
+		while (i < _quantity) {
+			*new_mov = *access_ptr;
+			new_mov++, access_ptr++, i++;
+		}
+
+		delete[] start_pos;
+		start_pos = new_array;
+		access_ptr = start_pos;
+
+		_size = _quantity;
+	}
+	
+	/// <summary>
+	/// Allows for the removal of an element at a specific index.
+	/// </summary>
+	/// <param name="idx">
+	/// The index to be removed.
+	/// </param>
+	void Remove(int idx) {
+		if (this->GetQuantity() == 0 || idx < 0 || idx >= _quantity) return;
+		T* new_array = new T[_quantity - 1];
+		T* new_mov = new_array;
+		access_ptr = start_pos;
+		int i = 0;
+
+		while (i < _quantity) {
+			if (i == idx) {
+				access_ptr++, i++;
+				continue;
+			}
+
+			*new_mov = *access_ptr;
+			new_mov++, access_ptr++, i++;
+		}
+
+		delete[] start_pos;
+		start_pos = new_array;
+		access_ptr = start_pos;
+		_size--, _quantity--;
+	}
+	
+	/// <summary>
+	/// Swaps the values at the two specified indices.
+	/// </summary>
+	/// <param name="start_idx">
+	/// The first element index you want to swap.
+	/// </param>
+	/// <param name="end_idx">
+	/// The second element index you want to swap.
+	/// </param>
+	void SwapIndices(int start_idx, int end_idx) {
+		if ((start_idx < 0 || start_idx >= this->GetQuantity()) || (end_idx < 0 || end_idx >= this->GetQuantity())) return;
+		T* index1 = nullptr;
+		T* index2 = nullptr;
+		int i = 0;
+
+		access_ptr = start_pos;
+		while (true) {
+			if (i == start_idx) index1 = access_ptr;
+			if (i == end_idx) index2 = access_ptr;
+
+			if (index1 != nullptr && index2 != nullptr) break;
+			access_ptr++;
+			i++;
+		}
+
+		T temp_num = *index1;
+		*index1 = *index2;
+		*index2 = temp_num;
+	}
+	
+	/// <summary>
+	/// Allows for insertion of a new element at a specific index.
+	/// </summary>
+	/// <param name="idx">
+	/// The index the new element will be in.
+	/// </param>
+	/// <param name="val">
+	/// The value at that new index.
+	/// </param>
+	void Insert(int idx, const T& val) {
+		if (idx < 0 || idx > _quantity) return;
+
+		T* new_array = new T[_quantity + 1];
+		T* new_mov = new_array;
+		access_ptr = start_pos;
+		int i = 0;
+
+		while (i <= idx - 1) {
+			*new_mov = *access_ptr;
+			access_ptr++, new_mov++, i++;
+		}
+		*new_mov = val;
+		i++, new_mov++;
+		while (access_ptr != (start_pos + _quantity)) {
+			*new_mov = *access_ptr;
+			access_ptr++, new_mov++, i++;
+		}
+
+		delete[] start_pos;
+		start_pos = new_array;
+		access_ptr = start_pos;
+		_quantity++;
+		_size = _quantity;
+	}
+
+// Element Access Operations
+	/// <summary>
+	/// Finds and returns the value at the specified index.
+	/// </summary>
+	/// <param name="idx">
+	/// The index to get the value from.
+	/// </param>
+	/// <returns>
+	/// The value at that index.
+	/// </returns>
+	T& FindAtIndex(int idx) {
+		return *(start_pos + idx);
+	}
+	
+	/// <summary>
+	/// Replaces the value at a specific index.
+	/// </summary>
+	/// <param name="idx">
+	/// The index to replace.
+	/// </param>
+	/// <param name="val">
+	/// The new value.
+	/// </param>
+	void ReplaceElement(int idx, const T& val) {
+		*(start_pos + idx) = val;
+	}
+	
+	/// <summary>
+	/// Allows for changing the value at an index using array notation.
+	/// </summary>
+	/// <param name="idx">
+	/// The index of the array to change.
+	/// </param>
+	/// <returns>
+	/// The pointer to the specific element of the array.
+	/// </returns>
+	T& operator[](int idx) {
+		return *(start_pos + idx);
+	}
+
+	/// <summary>
+	/// Accessor for a constant pointer
+	/// </summary>
+	/// <param name="idx">
+	/// The index of the array
+	/// </param>
+	/// <returns>
+	/// A constant pointer
+	/// </returns>
+	const T& operator[](int idx) const {
+		return *(start_pos + idx);
+	}
+	
+	/// <summary>
+	/// Attempts to find the index of a specific value in the array (only first occurence).
+	/// </summary>
+	/// <param name="val">
+	/// The value to be found.
+	/// </param>
+	/// <returns>
+	/// The index of that element in the vector. Returns last index if element not found.
+	/// </returns>
+	int FindElement(const T& val) {
+		int idx = 0;
+
+		while (idx < _quantity) {
+			if (*(start_pos + idx) == val) break;
+			idx++;
+		}
+
+		return idx;
+	}
+	
+	/// <summary>
+	/// Tells user if element is found in array.
+	/// </summary>
+	/// <param name="val">
+	/// Value to find.
+	/// </param>
+	/// <returns>
+	/// True if found, false if not found.
+	/// </returns>
+	bool Contains(const T& val) {
+		int i = 0;
+
+		while (i < _quantity) {
+			if (*(start_pos + i) == val) return true;
+			i++;
+		}
+
+		return false;
+	}
+
+// Internal Class Data Accessors
+	/// <summary>
+	/// Returns the size of the array.
+	/// </summary>
+	/// <returns>
+	/// Size of array.
+	/// </returns>
+	int GetSize() const {
+		return _size;
+	}
+	
+	/// <summary>
+	/// Returns the quantity of the array.
+	/// </summary>
+	/// <returns>
+	/// Current quantity of array.
+	/// </returns>
+	int GetQuantity() const {
+		return _quantity;
+	}
+	
+	/// <summary>
+	/// Getter method for a specific index pointer
+	/// </summary>
+	/// <param name="idx">
+	/// The index requested
+	/// </param>
+	/// <returns>
+	/// A pointer to a value
+	/// </returns>
+	T* GetPointer(int idx) {
+		return (start_pos + idx);
+	}
+	
+	/// <summary>
+	/// Determines if the array is empty.
+	/// </summary>
+	/// <returns>
+	/// True if empty, false if not empty.
+	/// </returns>
+	bool IsEmpty() {
+		if (_quantity == 0) return true;
+		else return false;
+	}
+	
+	/// <summary>
+	/// Clears the entire array by deleting and reallocating memory.
+	/// </summary>
+	void Clear() {
+		delete[] start_pos;
+		start_pos = new T[1], access_ptr = start_pos, _size = 1, _quantity = 0;
+	}
+	
+	/// <summary>
+	/// Prints all the elements in the array.
+	/// </summary>
+	void Print() {
+		for (int i = 0; i < this->GetQuantity(); i++) cout << "Element at Index " << i << ": " << FindAtIndex(i) << endl;
+	}
+
+// Sorting Algorithms
+	/// <summary>
+	/// Wrapper that the can be called to mergesort an array
+	/// [time complexity O(n log(n))]
+	/// </summary>
+	void MergeSort() {
+		int n = this->GetQuantity();
+		if (n <= 1) return;
+		DynamicArray<T> temp(n);
+		MergeSortRange(0, n - 1, temp);
+	}
+	
+	/// <summary>
+	/// Sorts the array using an insertion sort
+	/// [time complexity O(n^2)]
+	/// </summary>
+	void InsertionSort() {
+		if (this->GetQuantity() <= 1) return;
+		int i = 1;
+
+		while (i < this->GetQuantity()) {
+			for (int x = 1; x <= i; x++) {
+				if (this->FindAtIndex(x) < this->FindAtIndex(x - 1)) { 
+					i = x; 
+					break; 
+				}
+			}
+
+			int swap_idx = i;
+			while (this->FindAtIndex(swap_idx) < this->FindAtIndex(swap_idx - 1) && swap_idx > 0) {
+				this->SwapIndices(swap_idx, swap_idx - 1);
+				swap_idx--;
+			}
+			i++;
+		}
+	}
+	
+	/// <summary>
+	/// Sorts through the array, comparing adjacent elements and swapping as necessary
+	/// [time complexity O(n^2)]
+	/// </summary>
+	void BubbleSort() {
+		if (this->GetQuantity() <= 1) return;
+		int swap_count = 0;
+		do {
+			swap_count = 0;
+
+			for (int i = 1; i < this->GetQuantity(); i++) {
+				if (this->FindAtIndex(i) < this->FindAtIndex(i - 1)) {
+					this->SwapIndices(i, i - 1);
+					swap_count++;
+				}
+			}
+		} while (swap_count >= 1);
+	}
+	
+	/// <summary>
+	/// At each step, finds the smallest remaining unsorted element and puts it into the proper location
+	/// [time complexity O(n^2)]
+	/// </summary>
+	void SelectionSort() {
+		if (this->GetQuantity() <= 1) return;
+		int i = 0;
+		while (i < this->GetQuantity()) {
+			int smallest_idx = i;
+			for (int x = i; x < this->GetQuantity(); x++) {
+				if (this->FindAtIndex(x) < this->FindAtIndex(smallest_idx)) smallest_idx = x;
+			}
+
+			this->SwapIndices(i, smallest_idx);
+
+			i++, smallest_idx = 0;
+		}
+	}
 };
--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -1,89 +1,209 @@
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>  // std::sort
+
 #include "DynamicArray.h"
-using namespace std;
 
-int main(void) {
-	// testing merge sort
-	DynamicArray test;
-	cout << "Merge Sort Test:\n";
-	test.PushBack(5);
-	test.PushBack(7);
-	test.PushBack(3);
-	test.PushBack(9);
-	test.PushBack(4);
-	test.PushBack(6);
-	test.PushBack(1);
-	test.PushBack(10);
-	test.PushBack(2);
-	test.PushBack(8);
-	test.Print();
+// -----------------------------
+// Minimal test utilities
+// -----------------------------
+static int g_failCount = 0;
 
-	test.MergeSort();
-	cout << '\n';
-	test.Print();
-	cout << '\n';
+void fail(const std::string& msg) {
+    std::cerr << "[FAIL] " << msg << "\n";
+    g_failCount++;
+}
+void pass(const std::string& msg) {
+    std::cout << "[PASS] " << msg << "\n";
+}
 
-	// testing insertion sort
-	test.Clear();
-	cout << "Insertion Sort Test:\n";
-	test.PushBack(5);
-	test.PushBack(7);
-	test.PushBack(3);
-	test.PushBack(9);
-	test.PushBack(4);
-	test.PushBack(6);
-	test.PushBack(1);
-	test.PushBack(10);
-	test.PushBack(2);
-	test.PushBack(8);
-	test.Print();
+template <typename A, typename B>
+void assert_eq(const A& actual, const B& expected, const std::string& msg) {
+    if (!(actual == expected)) {
+        std::ostringstream oss;
+        oss << msg << " (expected: " << expected << ", got: " << actual << ")";
+        fail(oss.str());
+    }
+}
 
-	test.InsertionSort();
-	cout << '\n';
-	test.Print();
-	cout << '\n';
+template <typename T>
+DynamicArray<T> make_dynamic_array(const std::vector<T>& v) {
+    DynamicArray<T> a;
+    for (const auto& x : v) a.PushBack(x);
+    return a;
+}
 
-	// testing bubble sort
-	test.Clear();
-	cout << "Bubble Sort Test:\n";
-	test.PushBack(5);
-	test.PushBack(7);
-	test.PushBack(3);
-	test.PushBack(9);
-	test.PushBack(4);
-	test.PushBack(6);
-	test.PushBack(1);
-	test.PushBack(10);
-	test.PushBack(2);
-	test.PushBack(8);
-	test.Print();
+template <typename T>
+bool equals_sorted_result(DynamicArray<T>& a, const std::vector<T>& expectedSorted, std::string& why) {
+    if (a.GetQuantity() != (int)expectedSorted.size()) {
+        std::ostringstream oss;
+        oss << "quantity mismatch (expected " << expectedSorted.size()
+            << ", got " << a.GetQuantity() << ")";
+        why = oss.str();
+        return false;
+    }
 
-	test.BubbleSort();
-	cout << '\n';
-	test.Print();
-	cout << '\n';
+    for (int i = 0; i < (int)expectedSorted.size(); i++) {
+        // Use operator[] (present in your header)
+        T got = a[i];
+        if (!(got == expectedSorted[i])) {
+            std::ostringstream oss;
+            oss << "element mismatch at index " << i;
+            why = oss.str();
+            return false;
+        }
+    }
+    return true;
+}
 
-	// testing selection sort
-	test.Clear();
-	cout << "Selection Sort Test:\n";
-	test.PushBack(5);
-	test.PushBack(7);
-	test.PushBack(3);
-	test.PushBack(9);
-	test.PushBack(4);
-	test.PushBack(6);
-	test.PushBack(1);
-	test.PushBack(10);
-	test.PushBack(2);
-	test.PushBack(8);
-	test.Print();
+// -----------------------------
+// Sort runner wrappers
+// -----------------------------
+template <typename T>
+void run_one_sort_case(const std::string& sortName,
+    void (DynamicArray<T>::* sortFn)(),
+    const std::vector<T>& input,
+    const std::string& caseName) {
 
-	test.SelectionSort();
-	cout << '\n';
-	test.Print();
-	cout << '\n';
+    // Build expected reference
+    std::vector<T> expected = input;
+    std::sort(expected.begin(), expected.end());
 
-	test.~DynamicArray();
+    // Build your dynamic array and sort
+    DynamicArray<T> a = make_dynamic_array(input);
+    (a.*sortFn)();
 
-	return 0;
+    std::string why;
+    if (!equals_sorted_result(a, expected, why)) {
+        std::ostringstream oss;
+        oss << sortName << " | " << caseName << " | " << why;
+        fail(oss.str());
+        return;
+    }
+
+    pass(sortName + " | " + caseName);
+}
+
+template <typename T>
+void run_all_sorts_on_case(const std::vector<T>& input, const std::string& caseName) {
+    run_one_sort_case<T>("MergeSort", &DynamicArray<T>::MergeSort, input, caseName);
+    run_one_sort_case<T>("InsertionSort", &DynamicArray<T>::InsertionSort, input, caseName);
+    run_one_sort_case<T>("BubbleSort", &DynamicArray<T>::BubbleSort, input, caseName);
+    run_one_sort_case<T>("SelectionSort", &DynamicArray<T>::SelectionSort, input, caseName);
+}
+
+// -----------------------------
+// Test data sets
+// -----------------------------
+std::vector<int> ints_empty() { return {}; }
+std::vector<int> ints_one() { return { 42 }; }
+std::vector<int> ints_sorted() { return { -5, -1, 0, 2, 9, 13 }; }
+std::vector<int> ints_reverse() { return { 13, 9, 2, 0, -1, -5 }; }
+std::vector<int> ints_duplicates() { return { 5, 1, 3, 3, 2, 5, 1, 0, 0, -1, -1 }; }
+std::vector<int> ints_randomish() { return { 7, -3, 12, 0, 12, 4, -9, 1, 1, 8, -3, 6 }; }
+
+std::vector<std::string> strs_empty() { return {}; }
+std::vector<std::string> strs_one() { return { "one" }; }
+std::vector<std::string> strs_sorted() { return { "a", "b", "c", "d" }; }
+std::vector<std::string> strs_reverse() { return { "delta", "charlie", "bravo", "alpha" }; }
+std::vector<std::string> strs_duplicates() { return { "cat", "apple", "cat", "banana", "apple", "dog" }; }
+std::vector<std::string> strs_randomish() { return { "z", "aa", "a", "aaa", "ab", "b", "ba", "a" }; }
+
+// Custom struct test (ensures non-trivial type works)
+// NOTE: Your MergeSort uses <=, so we define both < and <=.
+struct Item {
+    int key;
+    std::string name;
+
+    bool operator<(const Item& other) const { return key < other.key; }
+    bool operator<=(const Item& other) const { return key <= other.key; }
+    bool operator==(const Item& other) const { return key == other.key && name == other.name; }
+};
+
+std::vector<Item> items_mixed() {
+    return {
+        {3, "c"},
+        {1, "a"},
+        {2, "b"},
+        {2, "b2"},
+        {5, "e"},
+        {4, "d"},
+        {1, "a2"}
+    };
+}
+
+// We need std::sort reference for Item: sort by key then name to match your comparison behavior.
+// Your sorts use only key comparisons (< / <=) as defined above (by key only).
+// That means items with same key may reorder differently depending on algorithm stability.
+// So we should compare ONLY by key ordering, not by exact pair ordering.
+//
+// We'll do a key-sorted check instead of full equality for Item.
+bool is_sorted_by_key(const DynamicArray<Item>& a) {
+    if (a.GetQuantity() <= 1) return true;
+    for (int i = 1; i < a.GetQuantity(); i++) {
+        if (a[i].key < a[i - 1].key) return false;
+    }
+    return true;
+}
+
+void test_all_sorts_items_case(const std::vector<Item>& input, const std::string& caseName) {
+    {
+        DynamicArray<Item> a = make_dynamic_array(input);
+        a.MergeSort();
+        if (!is_sorted_by_key(a)) fail("MergeSort | " + caseName + " | not sorted by key");
+        else pass("MergeSort | " + caseName);
+    }
+    {
+        DynamicArray<Item> a = make_dynamic_array(input);
+        a.InsertionSort();
+        if (!is_sorted_by_key(a)) fail("InsertionSort | " + caseName + " | not sorted by key");
+        else pass("InsertionSort | " + caseName);
+    }
+    {
+        DynamicArray<Item> a = make_dynamic_array(input);
+        a.BubbleSort();
+        if (!is_sorted_by_key(a)) fail("BubbleSort | " + caseName + " | not sorted by key");
+        else pass("BubbleSort | " + caseName);
+    }
+    {
+        DynamicArray<Item> a = make_dynamic_array(input);
+        a.SelectionSort();
+        if (!is_sorted_by_key(a)) fail("SelectionSort | " + caseName + " | not sorted by key");
+        else pass("SelectionSort | " + caseName);
+    }
+}
+
+// -----------------------------
+// Main test runner
+// -----------------------------
+int main() {
+    // ---- int cases ----
+    run_all_sorts_on_case<int>(ints_empty(), "int: empty");
+    run_all_sorts_on_case<int>(ints_one(), "int: one");
+    run_all_sorts_on_case<int>(ints_sorted(), "int: already sorted");
+    run_all_sorts_on_case<int>(ints_reverse(), "int: reverse sorted");
+    run_all_sorts_on_case<int>(ints_duplicates(), "int: duplicates");
+    run_all_sorts_on_case<int>(ints_randomish(), "int: random-ish");
+
+    // ---- string cases ----
+    run_all_sorts_on_case<std::string>(strs_empty(), "string: empty");
+    run_all_sorts_on_case<std::string>(strs_one(), "string: one");
+    run_all_sorts_on_case<std::string>(strs_sorted(), "string: already sorted");
+    run_all_sorts_on_case<std::string>(strs_reverse(), "string: reverse sorted");
+    run_all_sorts_on_case<std::string>(strs_duplicates(), "string: duplicates");
+    run_all_sorts_on_case<std::string>(strs_randomish(), "string: random-ish");
+
+    // ---- struct case (checks ordering by key; avoids stability assumptions) ----
+    test_all_sorts_items_case(items_mixed(), "Item: mixed keys + duplicates");
+
+    if (g_failCount == 0) {
+        std::cout << "\nALL SORT TESTS PASSED\n";
+        return 0;
+    }
+    else {
+        std::cout << "\nSORT TESTS FAILED. Count: " << g_failCount << "\n";
+        return 1;
+    }
 }
